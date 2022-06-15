54a7d95164b71a041462b5cf0c8b2de4fccf27b5
Madasish <madarsbatraks@gmail.com>
2022-06-15 17:50:48 +0300

    yeah fuck whatever i just made

--- index.ts
+++ index.ts
@@ -12,67 +12,6 @@ const client = new DiscordJS.Client({
 
 client.on('ready', () => {
     console.log('Ready!');
-
-    // 796269950024679425
-    const guildId = ``
-    const guild = client.guilds.cache.get(guildId);
-
-    let commands
-    
-    if (guild) {
-        commands = guild.commands
-    } else {
-        commands = client.application?.commands
-    }
-
-commands?.create({
-    name: 'ping',
-    description: 'Replies with pong!'
-})
-
-
-commands?.create({
-    name: 'add',
-    description: 'Adds two numbers together',
-    options: [
-        {
-            name: 'a',
-            description: 'First number',
-            type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
-            required: true
-        },
-        {
-            name: 'b',
-            description: 'First number',
-            type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
-            required: true
-        }
-    ]
 });
-
-
-});
-
-client.on('interactionCreate', async (interaction) => {
-    if(!interaction.isCommand()) return;
-
-    const { commandName, options } = interaction;
-
-    if(commandName === 'add') {
-        const a = options.getNumber('a')!;
-        const b = options.getNumber('b')!;
-        const sum = a + b;
-        interaction.reply(`The sum of ${a} and ${b} is ${sum}`);
-    }
-
-    if(commandName === 'ping') {
-        const ping = Math.round(client.ws.ping);
-        interaction.reply({
-            content: `Pong! (Heartbeat: ${ping}ms.)`,
-            ephemeral: true,
-        });
-    }
-
-})
 
 client.login(process.env.TOKEN);
