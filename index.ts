e8ef60346e51c4d3d3604d55773b5f3abafb92d0
Madasish <madarsbatraks@gmail.com>
2022-06-15 17:29:36 +0300

    added add

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env ts-node
 import DiscordJS, { ClientPresence, Intents } from 'discord.js';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -9,10 +10,11 @@ const client = new DiscordJS.Client({
     ]
 });
 
-client.on('ŗeady', () => {
+client.on('ready', () => {
     console.log('Ready!');
 
-    const guildId = `${process.env.TESTING_SERVER_ID}`
+    // 796269950024679425
+    const guildId = ``
     const guild = client.guilds.cache.get(guildId);
 
     let commands
@@ -28,38 +30,45 @@ commands?.create({
     description: 'Replies with pong!'
 })
 
-// commands?.create({
-//     name: 'add',
-//     description: 'Adds two numbers together',
-//     options: [
-//         {
-//             name: 'a',
-//             description: 'First number',
-//             type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
-//             required: true
-//         },
-//         {
-//             name: 'b',
-//             description: 'First number',
-//             type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
-//             required: true
-//         }
-//     ]
-// });
+
+commands?.create({
+    name: 'add',
+    description: 'Adds two numbers together',
+    options: [
+        {
+            name: 'a',
+            description: 'First number',
+            type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
+            required: true
+        },
+        {
+            name: 'b',
+            description: 'First number',
+            type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
+            required: true
+        }
+    ]
+});
 
 
 });
 
 client.on('interactionCreate', async (interaction) => {
-    if(!interaction.isCommand()) { 
-        return;
-    }
+    if(!interaction.isCommand()) return;
 
     const { commandName, options } = interaction;
 
+    if(commandName === 'add') {
+        const a = options.getNumber('a')!;
+        const b = options.getNumber('b')!;
+        const sum = a + b;
+        interaction.reply(`The sum of ${a} and ${b} is ${sum}`);
+    }
+
     if(commandName === 'ping') {
+        const ping = Math.round(client.ws.ping);
         interaction.reply({
-            content: 'pong',
+            content: `Pong! (Heartbeat: ${ping}ms.)`,
             ephemeral: true,
         });
     }
