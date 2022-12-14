763a2beb13231db5e7c06dadc47f8007faf39b4c
Madars Batraks <madarsbatraks@gmail.com>
2022-12-14 18:45:51 +0200

    + Added the ability to unpause using /play

--- commands/play.js
+++ commands/play.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { joinVoiceChannel, createAudioResource, createAudioPlayer, AudioPlayerStatus, getVoiceConnection} = require("@discordjs/voice")
 const ytdl = require('ytdl-core');
 const yts = require( 'yt-search' );
+// const util = require("")
 // const ch = require('chalk')
 
 module.exports = {
@@ -18,8 +19,6 @@ module.exports = {
 
         const channel = interaction.member.voice.channel
 
-        const query = interaction.options._hoistedOptions[0].value
-
         await interaction.deferReply({
             ephemeral: false
         })
@@ -30,11 +29,13 @@ module.exports = {
         // Check if a queue exists for the server youre in
         if(!map.get(channel.guild.id)) {
 
+            if(interaction.options._hoistedOptions.length < 1) return interaction.editReply("You need to provide a query!")
+
             // if(!interaction.options._hoistedOptions) return interaction.editReply("You must provide a query!")
 
             console.log("Music queue doesnt exist")
 
-            const results = await yts({query: query, type: "video"})
+            const results = await yts({query: interaction.options._hoistedOptions[0].value, type: "video"})
             const result = results.videos[0]
 
             map.set(channel.guild.id, {
@@ -48,12 +49,19 @@ module.exports = {
             console.log("Music queue created")
             
             const getNextSong = function() {
-                const array = gqGet.queue.slice(1,0)
-                map.set(channel.guild.id.queue, array)
+
+                // const map = queueMap.get(channel.guild.id)
+                // map.loop = true
+                // queueMap.set(channel.guild.id, map)
+
+                const the = interaction.client.queueMap.get(interaction.guildId)
+                console.log(the)
+                the.queue = the.queue.slice(1)
+                console.log(the)
+                interaction.client.queueMap.set(channel.guild.id, the)
+
                 return createAudioResource(ytdl(gqGet.queue[0].url))
             }
-            
-            // map.set(channel.guild.id.resource, createAudioPlayer(ytdl(gqGet.queue[0].url)))
 
             const connection = joinVoiceChannel({
                 channelId: channel.id,
@@ -77,6 +85,7 @@ module.exports = {
                 console.log(`Idling in ${interaction.guildId}`)
 
                 if(gqGet.loop == true) {
+                    console.log(`Looping in ${interaction.guildId}`)
                     player.play(createAudioResource(ytdl(gqGet.queue[0].url)))
                     return
                 } else {
@@ -96,14 +105,30 @@ module.exports = {
             await interaction.editReply(`Playing "${gqGet.queue[0].title}"`)
             
         } else if(map.get(channel.guild.id).queue) {
-            if(getVoiceConnection(channel.guild.id)._state.subscribtion.player._state == "paused") {
-                getVoiceConnection(channel.guild.id)._state.subscribtion.player.unpause()
+
+            console.log("queue exists")
+
+            if(interaction.options._hoistedOptions.length < 1) {
+                const connection = getVoiceConnection(channel.guild.id)
+            
+                if(!connection || connection._state.subscription.player._state.status == "idle") return interaction.editReply({content: "There is nothing to unpause!"})
+
+                // Check if player is playing
+                if(connection._state.subscription.player._state.status == "playing") return interaction.editReply("Already playing!")
+
+                // Check if the member is in the same vc as the bot
+                if(interaction.member.voice.channel != getVoiceConnection(interaction.guild.id).joinConfig.channelId) return interaction.editReply("You need to be in the same voice channel as the bot!")
+
+                // Unpause
+                connection._state.subscription.player.unpause()
+
+                return interaction.editReply("Resumed playback!")
+                // ._state.subscription.player._state.status
             } else {
-                console.log("queue exists")
                 
                 if(channel != getVoiceConnection(channel.guild.id).joinConfig.channelId) return interaction.editReply("You need to be in the same voice channel as the bot!")
 
-                const results = await yts({query: query, type: "video"})
+                const results = await yts({query: interaction.options._hoistedOptions[0].value, type: "video"})
                 const result = results.videos[0]
                 const video = map.get(channel.guild.id).queue.push(result)
                 map.set(channel.guild.id.queue, video)
@@ -111,6 +136,7 @@ module.exports = {
                 console.log(`Added "${result.title}" to the queue in ${interaction.guildId}`)
 
                 await interaction.editReply(`Added "${result.title}" to the queue!`)
+
             }
             
         } else {
